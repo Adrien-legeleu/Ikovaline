d416d57dd7548477978b874d570f128a743fcb66
Test <adrienlegeleu@gmail.com>
2025-01-16 23:01:16 +0100

    ikovaline

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,6 +1,6 @@
 import { EmailTemplate } from "@/components/email-template";
 import { Resend } from "resend";
-
+console.log(process.env.RESEND_API_KEY);
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
--- components/email-template.tsx
+++ components/email-template.tsx
@@ -1,5 +1,5 @@
+import LogoImg from "@/public/images/logo/ikovaline_logo.png";
 import Image from "next/image";
-import * as React from "react";
 
 interface EmailTemplateProps {
   firstName: string;
@@ -10,75 +10,71 @@ interface EmailTemplateProps {
   category: string;
 }
 
-export const EmailTemplate = ({
+export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   firstName,
   lastName,
   email,
   tel,
   message,
   category,
-}: EmailTemplateProps) => {
-  return (
-    <div className="max-w-2xl font-poppins mx-auto p-6">
-      <div className="bg-white shadow-lg rounded-lg p-6">
-        {/* Logo Ikovaline */}
-        <div className="mb-4">
-          <Image
-            src="/static/ikovaline_logo.png" // Assure-toi que le logo est dans le dossier public
-            alt="Ikovaline"
-            className="w-32"
-            width={50}
-            height={50}
-          />
-        </div>
+}) => (
+  <div className="max-w-2xl font-poppins mx-auto p-6">
+    <div className="bg-white shadow-lg rounded-lg p-6">
+      {/* Logo Ikovaline */}
+      <div className="mb-4">
+        <Image
+          src="/static/ikovaline_logo.png" // Assure-toi que le logo est dans le dossier public
+          alt="Ikovaline"
+          className="w-32"
+          width={50}
+          height={50}
+        />
+      </div>
 
-        {/* Header */}
-        <h1 className="text-2xl font-bold font-poppins text-gray-800">
-          Nouveau message reçu de {firstName} {lastName}
-        </h1>
-        <p className="text-gray-600 mt-2">
-          Catégorie : <span className="font-semibold">{category}</span>
+      {/* Header */}
+      <h1 className="text-2xl font-bold font-poppins text-gray-800">
+        Nouveau message reçu de {firstName} {lastName}
+      </h1>
+      <p className="text-gray-600 mt-2">
+        Catégorie : <span className="font-semibold">{category}</span>
+      </p>
+
+      {/* Détails du contact */}
+      <div className="mt-6 bg-neutral-200 rounded-2xl p-4">
+        <h2 className="text-xl font-semibold text-gray-800">
+          Détails du client :
+        </h2>
+        <ul className="mt-2 text-gray-700">
+          <li>
+            <span className="font-medium">Prénom :</span> {firstName}
+          </li>
+          <li>
+            <span className="font-medium">Nom :</span> {lastName}
+          </li>
+          <li>
+            <span className="font-medium">Email :</span>{" "}
+            <a
+              href={`mailto:${email}`}
+              className="text-blue-600 hover:underline"
+            >
+              {email}
+            </a>
+          </li>
+          <li>
+            <span className="font-medium">Téléphone :</span>{" "}
+            <a href={`tel:${tel}`} className="text-blue-600 hover:underline">
+              {tel}
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mt-6">
+        <h2 className="text-xl font-semibold text-gray-800">Message :</h2>
+        <p className="mt-2 text-gray-700">
+          {message ? message : "Aucun message fourni"}
         </p>
-
-        {/* Détails du contact */}
-        <div className="mt-6 bg-neutral-200 rounded-2xl p-4">
-          <h2 className="text-xl font-semibold text-gray-800">
-            Détails du client :
-          </h2>
-          <ul className="mt-2 text-gray-700">
-            <li>
-              <span className="font-medium">Prénom :</span> {firstName}
-            </li>
-            <li>
-              <span className="font-medium">Nom :</span> {lastName}
-            </li>
-            <li>
-              <span className="font-medium">Email :</span>{" "}
-              <a
-                href={`mailto:${email}`}
-                className="text-blue-600 hover:underline"
-              >
-                {email}
-              </a>
-            </li>
-            <li>
-              <span className="font-medium">Téléphone :</span>{" "}
-              <a href={`tel:${tel}`} className="text-blue-600 hover:underline">
-                {tel}
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="mt-6">
-          <h2 className="text-xl font-semibold text-gray-800">Message :</h2>
-          <p className="mt-2 text-gray-700">
-            {message ? message : "Aucun message fourni"}
-          </p>
-        </div>
       </div>
     </div>
-  );
-};
-
-export default EmailTemplate;
+  </div>
+);
