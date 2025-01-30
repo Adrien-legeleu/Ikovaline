a701b38d4ce0c3090fe9602a49292d753eec5592
Test <adrienlegeleu@gmail.com>
2025-01-30 14:41:58 +0100

    ikovaline

--- components/LandingPage/review/Review.tsx
+++ components/LandingPage/review/Review.tsx
@@ -133,8 +133,8 @@ export function Review() {
             <ReviewCard key={review.username} {...review} />
           ))}
         </Marquee>
-        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
-        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
+        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-[#F4FAFB] dark:from-background"></div>
+        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-[#F4FAFB] dark:from-background"></div>
       </div>
     </div>
   );
