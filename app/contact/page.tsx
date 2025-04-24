466b9c858cd467114d061334c82ce125dfb59976
Test <adrienlegeleu@gmail.com>
2025-04-24 21:38:06 +0200

    iovaline

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,18 +1,18 @@
 import ContactMainPage from "@/components/ContactPage/ContactMainPage";
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: "Contact - Ikovaline | Discutons de votre projet digital",
-//   description:
-//     "Besoin d'un accompagnement digital sur mesure ? Contactez Ikovaline, votre partenaire en marketing numérique pour entreprises et étudiants.",
-//   openGraph: {
-//     title: "Contactez Ikovaline | Conseil et solutions digitales",
-//     description:
-//       "Prenez contact avec notre équipe pour booster votre visibilité, améliorer votre stratégie ou rejoindre une aventure étudiante passionnante.",
-//     url: "https://ikovaline.com/contact",
-//     type: "website",
-//   },
-// };
+export const metadata: Metadata = {
+  title: "Contact - Ikovaline | Discutons de votre projet digital",
+  description:
+    "Besoin d'un accompagnement digital sur mesure ? Contactez Ikovaline, votre partenaire en marketing numérique pour entreprises et étudiants.",
+  openGraph: {
+    title: "Contactez Ikovaline | Conseil et solutions digitales",
+    description:
+      "Prenez contact avec notre équipe pour booster votre visibilité, améliorer votre stratégie ou rejoindre une aventure étudiante passionnante.",
+    url: "https://ikovaline.com/contact",
+    type: "website",
+  },
+};
 
 export default function Page() {
   return <ContactMainPage />;
