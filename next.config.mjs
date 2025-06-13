5eff1f8f9a1eb34a998dadaccaa081218516922c
Test <adrienlegeleu@gmail.com>
2025-06-13 16:08:31 +0200

    ikova

--- next.config.mjs
+++ next.config.mjs
@@ -8,21 +8,6 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "avatar.vercel.sh"],
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.ikovaline.com",
-          },
-        ],
-        destination: "https://ikovaline.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
