cbd4d90a85836b72fb77aade0ffb2fbaf0209fec
Adrien Legeleux <adrienlegeleu@gmail.com>
2025-09-12 23:06:08 +0200

    ikovalie

--- components/ServicesPage/servicesComponents/Service2.tsx
+++ components/ServicesPage/servicesComponents/Service2.tsx
@@ -4,7 +4,6 @@
 import * as React from 'react';
 import Link from 'next/link';
 import { IconArrowRight } from '@tabler/icons-react';
-import { motion } from 'framer-motion';
 
 type Feature = {
   title: string;
