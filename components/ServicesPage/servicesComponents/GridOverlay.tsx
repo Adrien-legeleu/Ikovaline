5baad0355c47bf3a3b03bba81dfaa03e618f3d3d
Adrien Legeleux <adrienlegeleu@gmail.com>
2025-09-27 14:56:36 +0200

    ikovaline

--- components/ServicesPage/servicesComponents/GridOverlay.tsx
+++ components/ServicesPage/servicesComponents/GridOverlay.tsx
@@ -4,11 +4,11 @@ export function Background() {
   return (
     <div
       aria-hidden
-      className="absolute inset-0 z-0 [mask-image:radial-gradient(circle_at_center,white_0%,white_32%,transparent_70%)]"
+      className="absolute inset-0 z-0 [mask-image:radial-gradient(circle_at_center,white_0%,white_32%,transparent_70%)] dark:[mask-image:radial-gradient(circle_at_center,#000000_0%,#000000_32%,transparent_70%)]"
     >
       {/* bandes verticales subtiles */}
       <div
-        className="absolute inset-0"
+        className="absolute dark:opacity-0 inset-0"
         style={{
           background:
             'repeating-linear-gradient(90deg, rgba(241,245,249,0.7) 0 30px, rgba(255,255,255,1) 80px 80px)',
@@ -19,12 +19,12 @@ export function Background() {
         className="absolute inset-0 dark:opacity-100 opacity-0 mix-blend-normal dark:mix-blend-normal"
         style={{
           background:
-            'repeating-linear-gradient(90deg, rgba(23,23,23,0.7) 0 30px, rgba(10,10,10,1) 80px 80px)',
+            'repeating-linear-gradient(90deg, rgba(23,23,23,0.9) 0 30px, rgba(10,10,10,1) 80px 80px)',
         }}
       />
       {/* bruit léger */}
       <div
-        className="absolute inset-0 h-full w-full scale-[1.2] opacity-[0.05] dark:opacity-[0.07] [mask-image:radial-gradient(#fff,transparent,75%)]"
+        className="absolute inset-0 h-full w-full scale-[1.2] opacity-[0.05] dark:opacity-[0.07] [mask-image:radial-gradient(#fff,transparent,75%)] dark:[mask-image:radial-gradient(#000,transparent,75%)]"
         style={{ backgroundImage: 'url(/noise.webp)', backgroundSize: '20%' }}
       />
     </div>
